131ebe2e800537d3f6ad94825f2b53b761c98a0c
Anna Dlubis <dlubis.ania@gmail.com>
2023-03-03 21:10:57 +0100

    Solved problem 2.

---
+++ problem2.py
@@ -0,0 +1,31 @@
+def suma_fibonacci(liczba):
+
+    krok1 = 1 
+    krok2 = 2
+    krok3 = 0
+    suma_ciagu = 2
+    while True:
+        #obliczenie kolejnego elementu ciągu fibonacciego
+        krok3 = krok2 + krok1
+        if krok3 >= liczba:
+            break
+        krok1 = krok2
+        krok2 = krok3
+        #print(krok3)
+
+        # obliczenie sumy parzystych elementów ciągu fibonacciego
+        if krok3 % 2 == 0:
+            suma_ciagu = suma_ciagu + krok3
+
+    return(suma_ciagu)
+
+
+def main():
+    max = 4000000
+    print(suma_fibonacci(max))
+
+
+if __name__ == "__main__":
+    main()
+
+
